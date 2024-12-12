e06ba80f6897d7144fe3b65f8a8c0c7e1f556dae
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2024-12-12 15:49:27 +0100

    pt1 working

--- 2024/12_garden_groups/aoc202412.py
+++ 2024/12_garden_groups/aoc202412.py
@@ -7,10 +7,54 @@ import sys
 
 def parse_data(puzzle_input):
     """Parse input."""
+    return [list(line) for line in puzzle_input.strip().split('\n')]
 
 
 def part1(data):
     """Solve part 1."""
+    visited = set()
+
+    total = 0
+    for i, row in enumerate(data):
+        for j, cell in enumerate(row):
+            if (i, j) in visited:
+                continue
+            this_set = {}
+            find_all_neighbors(data, i, j, this_set, cell)
+            print(cell, this_set)
+            for key, value in this_set.items():
+                visited.add(key)
+                total += value*len(this_set)
+
+    return total
+
+def find_all_neighbors(data,  i, j, this_set, curr):
+    if (i, j) in this_set:
+        return
+
+    """Find all neighbors."""
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # Up, Right, Down, Left
+    rating = 4
+
+    for direction in directions:
+        nx = i + direction[0]
+        ny = j + direction[1]
+        if ny < 0 or ny >= len(data) or nx < 0 or nx >= len(data[0]):
+            continue
+        if data[nx][ny] != curr:
+            continue
+        rating -= 1
+
+    this_set[(i, j)] = rating
+
+    for direction in directions:
+        nx = i + direction[0]
+        ny = j + direction[1]
+        if ny < 0 or ny >= len(data) or nx < 0 or nx >= len(data[0]):
+            continue
+        if data[nx][ny] != curr:
+            continue
+        find_all_neighbors(data, nx, ny, this_set, curr)
 
 
 def part2(data):
