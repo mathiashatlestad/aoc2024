1eb10ecf4dc7acd6fd68083de202f208eabfd611
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2024-12-13 01:08:33 +0100

    wip

--- 2024/12_garden_groups/aoc202412.py
+++ 2024/12_garden_groups/aoc202412.py
@@ -21,10 +21,9 @@ def part1(data):
                 continue
             this_set = {}
             find_all_neighbors(data, i, j, this_set, cell)
-            print(cell, this_set)
             for key, value in this_set.items():
                 visited.add(key)
-                total += value*len(this_set)
+                total += len(this_set)*value
 
     return total
 
@@ -56,10 +55,91 @@ def find_all_neighbors(data,  i, j, this_set, curr):
             continue
         find_all_neighbors(data, nx, ny, this_set, curr)
 
+def scan_horizontal(this_set, i, j_min, j_max):
+    left_edge = 0
+    right_edge = 0
+    already_right_edge = set()
+    already_left_edge = set()
+    for j in range(j_min - 1, j_max + 1):
+
+        if (i, j) not in this_set:
+            continue
+
+        ny = j - 1
+
+        if (i - 1, j) not in this_set and (i, ny) not in already_left_edge:
+            left_edge+=1
+            already_left_edge.add((i, j))
+
+        if (i + 1, j) not in this_set and (i, ny) not in already_right_edge:
+            right_edge+=1
+            already_right_edge.add((i, j))
+
+    return right_edge + left_edge
+
+
+def scan_vertical(this_set, j, i_min, i_max):
+    left_edge = 0
+    right_edge = 0
+    already_right_edge = set()
+    already_left_edge = set()
+    for i in range(i_min - 1, i_max + 1):
+        if (i, j) not in this_set:
+            continue
+
+        ny = i - 1
+        if (i, j - 1) not in this_set and (ny, j) not in already_left_edge:
+            left_edge += 1
+            already_left_edge.add((i, j))
+
+        if (i, j + 1) not in this_set and (ny, j) not in already_right_edge:
+            right_edge += 1
+            already_right_edge.add((i, j))
+
+    return right_edge + left_edge
+
 
 def part2(data):
-    """Solve part 2."""
+    visited = set()
+    total = 0
+    for i, row in enumerate(data):
+        for j, cell in enumerate(row):
+            if (i, j) in visited:
+                continue
 
+            this_set = {}
+            find_all_neighbors(data, i, j, this_set, cell)
+            for key, value in this_set.items():
+                visited.add(key)
+
+            all_i = [t[0] for t in this_set.keys()]
+            all_j = [t[1] for t in this_set.keys()]
+
+            global_min_i = min(all_i)
+            global_max_i = max(all_i)
+            global_min_j = min(all_j)
+            global_max_j = max(all_j)
+
+            horizontal = 0
+            print("First: " + cell)
+            print("------")
+            for ti in range(global_min_i, global_max_i + 1):
+                horizontal += scan_horizontal(this_set, ti, global_min_j, global_max_j)
+
+            print("------")
+            vertical = 0
+            for ji in range(global_min_j, global_max_j + 1):
+                vertical+=scan_vertical(this_set, ji, global_min_i, global_max_i)
+            print("------")
+
+            print(cell, len(this_set), horizontal, vertical)
+
+            print("------")
+            print("------")
+
+
+            total += len(this_set)*(vertical + horizontal)
+    return total
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
