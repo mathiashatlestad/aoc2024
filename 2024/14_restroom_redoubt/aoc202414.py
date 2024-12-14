5b6d205f1aa15a32d09299d70938ef7bbf9915c5
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2024-12-14 21:36:46 +0100

    pt2

--- 2024/14_restroom_redoubt/aoc202414.py
+++ 2024/14_restroom_redoubt/aoc202414.py
@@ -1,9 +1,9 @@
 """AoC 14, 2024: Restroom Redoubt."""
-
+import functools
 # Standard library imports
 import pathlib
 import sys
-
+from PIL import Image
 
 def parse_data(puzzle_input):
     """Parse input."""
@@ -15,6 +15,7 @@ def parse_data(puzzle_input):
         result.append(((px, py), (vx, vy)))
     return result
 
+@functools.lru_cache(maxsize=None)
 def resulting_position(pos, vel, max_pos, iterations):
     res_x = (pos[0]+iterations*vel[0])%max_pos[0]
     if res_x < 0:
@@ -24,10 +25,9 @@ def resulting_position(pos, vel, max_pos, iterations):
     if res_y < 0:
         res_y = max_pos[1] - res_y
 
-    return res_x, res_y
+    return (res_x, res_y)
 
 def calculate_weight(results, max_pos):
-
     first = 0
     for x in range(0, (max_pos[0] // 2)):
         for y in range(0, (max_pos[1] // 2)):
@@ -50,33 +50,31 @@ def calculate_weight(results, max_pos):
 
     return first * second * third * forth
 
-
 def part1(data):
     """Solve part 1."""
-    print(data)
     result = []
     max_pos = (101, 103)
     for item in data:
         result.append(resulting_position(item[0], item[1], max_pos, 100))
-
-    print(result)
-    array = ""
-    for j in range(max_pos[1]):
-        tmp = ""
-        for i in range(max_pos[0]):
-            if (i,j) in result:
-                tmp+="x"
-            else:
-                tmp += "."
-        array += tmp + "\n"
-
-    print(array)
-
     return calculate_weight(result, max_pos)
 
 
 def part2(data):
     """Solve part 2."""
+    max_pos = (101, 103)
+    iterations = 0
+
+    for _ in range(10000):
+        img = Image.new('RGB', (103, 103), "white")
+        pix = img.load()
+        for index, item in enumerate(data):
+            res = resulting_position(item[0], item[1], max_pos, 1)
+            data[index] = (res, item[1])
+            pix[res[0], res[1]] = (255, 0, 0)
+        iterations += 1
+        img.save("pictures/" + str(iterations) + ".png")
+
+    return iterations
 
 
 def solve(puzzle_input):
