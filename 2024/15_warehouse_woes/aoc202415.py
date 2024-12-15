ac4f61a7436fa888e5f883f7c650c54be9176605
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2024-12-15 15:29:51 +0100

    improvements

--- 2024/15_warehouse_woes/aoc202415.py
+++ 2024/15_warehouse_woes/aoc202415.py
@@ -4,6 +4,8 @@
 import pathlib
 import sys
 
+directions = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
+
 def parse_data(puzzle_input):
     """Parse input."""
     parts = puzzle_input.strip().split("\n\n")
@@ -11,9 +13,22 @@ def parse_data(puzzle_input):
     moves = list(parts[1].replace('\n', ''))
     return mapp, moves
 
-def try_move(mapp, start, direction):
+def calculate_weights(mapp):
+    value = 0
+    for i, row in enumerate(mapp):
+        for j, col in enumerate(row):
+            if col == 'O' or col == '[':
+                value += i*100 + j
+    return value
+
+def find_start(mapp):
+    for i, row in enumerate(mapp):
+        for j, col in enumerate(row):
+            if col == '@':
+                return (i, j)
+
+def try_move_pt1(mapp, start, direction):
     current = start
-    cleared_to_move = False
     boxes_to_move = []
     while True:
         ni, nj = current[0] + direction[0], current[1] + direction[1]
@@ -47,7 +62,6 @@ def try_move(mapp, start, direction):
 def find_if_allowed_to_move_boxes(mapp, start, direction):
     boxes_to_move = []
     ni, nj = start[0] + direction[0], start[1] + direction[1]
-    all_allowed = True
 
     if mapp[ni][nj] == '[':
         boxes_to_move.append((ni, nj))
@@ -58,7 +72,7 @@ def find_if_allowed_to_move_boxes(mapp, start, direction):
 
     box_added = True
 
-    while box_added and all_allowed:
+    while box_added:
         box_added = False
         for box in boxes_to_move:
             ni, nj = box[0] + direction[0], box[1] + direction[1]
@@ -80,7 +94,7 @@ def find_if_allowed_to_move_boxes(mapp, start, direction):
 
     return boxes_to_move
 
-def try_move_v2(mapp, start, direction):
+def try_move_pt2(mapp, start, direction):
     current = start
     ni, nj = current[0] + direction[0], current[1] + direction[1]
 
@@ -105,38 +119,7 @@ def try_move_v2(mapp, start, direction):
 
     return start
 
-def part1(mapp, moves):
-    """Solve part 1."""
-    return None
-
-    directions = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
-    current = (0, 0)
-    for i, row in enumerate(mapp):
-        for j, col in enumerate(row):
-            if col == '@':
-                current = (i, j)
-                break
-
-    for move in moves:
-        current = try_move(mapp, current, directions[move])
-
-    value = 0
-    for i, row in enumerate(mapp):
-        for j, col in enumerate(row):
-            if col == 'O':
-                value += i*100 + j
-
-    if value == 1318523:
-        print("PART 1 DONE")
-
-    return value
-
-import copy
-
-def part2(mapp, moves):
-    """Solve part 2."""
-    print_mapp(mapp)
-
+def extend_map_pt2(mapp):
     new_mapp = []
     for i, row in enumerate(mapp):
         adjusted = []
@@ -154,49 +137,31 @@ def part2(mapp, moves):
                 adjusted.append('[')
                 adjusted.append(']')
         new_mapp.append(adjusted)
-    mapp = new_mapp
+    return new_mapp
 
-    print("Initial map")
-    print_mapp(mapp)
-
-    directions = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
-    current = (0, 0)
-    for i, row in enumerate(mapp):
-        for j, col in enumerate(row):
-            if col == '@':
-                current = (i, j)
-                break
-
+def part1(puzzle_input):
+    """Solve part 1."""
+    mapp, moves = parse_data(puzzle_input)
+    current = find_start(mapp)
     for move in moves:
-        current = try_move_v2(mapp, current, directions[move])
+        current = try_move_pt1(mapp, current, directions[move])
+    return calculate_weights(mapp)
 
-    value = 0
-    for i, row in enumerate(mapp):
-        for j, col in enumerate(row):
-            if col == '[':
-                value += i * 100 + j
 
-    print_mapp(mapp)
 
-    if value == 1342227:
-        print("PART 2 WRONG")
-
-    return value
-
-def print_mapp(mapp):
-    result = ""
-    for i, row in enumerate(mapp):
-        tmp = ""
-        for j, col in enumerate(row):
-            tmp += col
-        result += tmp + '\n'
-    print(result)
+def part2(puzzle_input):
+    """Solve part 2."""
+    mapp, moves = parse_data(puzzle_input)
+    mapp = extend_map_pt2(mapp)
+    current = find_start(mapp)
+    for move in moves:
+        current = try_move_pt2(mapp, current, directions[move])
+    return calculate_weights(mapp)
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
-    data, moves = parse_data(puzzle_input)
-    yield part1(data, moves)
-    yield part2(data, moves)
+    yield part1(puzzle_input)
+    yield part2(puzzle_input)
 
 
 if __name__ == "__main__":
