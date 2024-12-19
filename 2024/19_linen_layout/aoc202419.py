b45c5b053d3311febb73895224eb79561b1482af
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2024-12-19 12:15:28 +0100

    minimize the hell out of it

--- 2024/19_linen_layout/aoc202419.py
+++ 2024/19_linen_layout/aoc202419.py
@@ -1,48 +1,12 @@
-"""AoC 19, 2024: Linen Layout."""
 import functools
-# Standard library imports
 import pathlib
 import sys
 
-sys.setrecursionlimit(10 ** 6)
-
-patterns = []
-
-def parse_data(puzzle_input):
-    """Parse input."""
-    global patterns
-    patterns_section, designs_section = puzzle_input.strip().split("\n\n", 1)
-    patterns = [p.strip() for p in patterns_section.split(",")]
-    designs = designs_section.strip().splitlines()
-    return designs
-
-
 @functools.lru_cache(maxsize=None)
-def is_possible_count(design):
-    return 1 if not design else sum(
-        is_possible_count(design[len(pattern):])
-        for pattern in patterns
-        if design.startswith(pattern)
-    )
+def solve(design):
+    return 1 if not design else sum(solve(design[len(p):]) for p in patterns if design.startswith(p))
 
-def part1(designs):
-    """Solve part 1."""
-    return sum(is_possible_count(design) > 0 for design in designs)
-
-def part2(designs):
-    """Solve part 2."""
-    return sum(is_possible_count(design) for design in designs)
-
-
-def solve(puzzle_input):
-    """Solve the puzzle for the given input."""
-    designs = parse_data(puzzle_input)
-    yield part1(designs)
-    yield part2(designs)
-
-
-if __name__ == "__main__":
-    for path in sys.argv[1:]:
-        print(f"\n{path}:")
-        solutions = solve(puzzle_input=pathlib.Path(path).read_text().rstrip())
-        print("\n".join(str(solution) for solution in solutions))
+patterns_section, designs = pathlib.Path(sys.argv[1]).read_text().strip().split("\n\n")
+patterns = [p.strip() for p in patterns_section.split(",")]
+print(sum(solve(d) > 0 for d in designs.strip().splitlines())) # Part 1
+print(sum(solve(d) for d in designs.strip().splitlines())) # Part 2
