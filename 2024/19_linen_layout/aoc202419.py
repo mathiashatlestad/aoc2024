eac6c3256f804f3563c14658d31cb4b1a708e10b
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2024-12-19 12:03:16 +0100

    pythonic

--- 2024/19_linen_layout/aoc202419.py
+++ 2024/19_linen_layout/aoc202419.py
@@ -10,62 +10,35 @@ patterns = []
 
 def parse_data(puzzle_input):
     """Parse input."""
-    parts = puzzle_input.split("\n\n")
-
     global patterns
-    patterns = [pattern.strip() for pattern in parts[0].strip().split(",")]
-
-    designs = parts[1].strip().split("\n")
+    patterns_section, designs_section = puzzle_input.strip().split("\n\n", 1)
+    patterns = [p.strip() for p in patterns_section.split(",")]
+    designs = designs_section.strip().splitlines()
     return designs
 
-@functools.lru_cache(maxsize=None)
-def is_possible(design):
-    for pattern in patterns:
-        if pattern == design:
-            return True
-        if design.startswith(pattern):
-            if is_possible(design[:len(pattern)]) and (len(pattern) >= len(design) or is_possible(design[len(pattern):])):
-                return True
-
-    return False
 
 @functools.lru_cache(maxsize=None)
 def is_possible_count(design):
-    permutation = 0
-    for pattern in patterns:
-        if pattern == design:
-            permutation += 1
-        elif design.startswith(pattern):
-            permutation += is_possible_count(design[:len(pattern)])
-            permutation += is_possible_count(design[len(pattern):]) if len(design) > len(pattern) else 0
-    return permutation
+    return 1 if not design else sum(
+        is_possible_count(design[len(pattern):])
+        for pattern in patterns
+        if design.startswith(pattern)
+    )
 
-def part1(data):
+def part1(designs):
     """Solve part 1."""
-    designs = parse_data(data)
-    print(patterns)
-    print(designs)
-    count = 0
-    for design in designs:
-        if is_possible(design):
-            count += 1
-    return count
+    return sum(is_possible_count(design) > 0 for design in designs)
 
-def part2(data):
+def part2(designs):
     """Solve part 2."""
-    designs = parse_data(data)
-    print(patterns)
-    print(designs)
-    count = 0
-    for design in designs:
-        count += is_possible_count(design)
-    return count
+    return sum(is_possible_count(design) for design in designs)
 
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
-    yield part1(puzzle_input)
-    yield part2(puzzle_input)
+    designs = parse_data(puzzle_input)
+    yield part1(designs)
+    yield part2(designs)
 
 
 if __name__ == "__main__":
