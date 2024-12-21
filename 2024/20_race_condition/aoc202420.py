12cd58f4ebc1b58c772dcfbabd2ee063fe4da1d8
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2024-12-21 20:44:03 +0100

    a little conversation with chatgpt help turned me to use bfs all to all intead of diikstra

--- 2024/20_race_condition/aoc202420.py
+++ 2024/20_race_condition/aoc202420.py
@@ -3,9 +3,9 @@ import pathlib
 from collections import deque
 
 DIRECTIONS = [
-    (0, 1),   # down
+    (0, 1),  # down
     (0, -1),  # up
-    (1, 0),   # right
+    (1, 0),  # right
     (-1, 0),  # left
 ]
 
@@ -16,6 +16,7 @@ def parse_data(puzzle_input):
         lines = puzzle_input
     return [list(line) for line in lines if line]
 
+
 def find_item(mapp, c):
     for r, row in enumerate(mapp):
         for c_idx, val in enumerate(row):
@@ -23,43 +24,30 @@ def find_item(mapp, c):
                 return (r, c_idx)
     return None
 
-def bfs_normal(mapp, start):
+
+def bfs(mapp, start, ignore_walls=False, max_steps=None):
     rows, cols = len(mapp), len(mapp[0])
-    dist = [[None]*cols for _ in range(rows)]
+    dist = [[None] * cols for _ in range(rows)]
     queue = deque()
     r0, c0 = start
     dist[r0][c0] = 0
     queue.append((r0, c0))
+
     while queue:
         r, c = queue.popleft()
         for dr, dc in DIRECTIONS:
-            nr, nc = r+dr, c+dc
+            nr, nc = r + dr, c + dc
             if 0 <= nr < rows and 0 <= nc < cols:
-                if mapp[nr][nc] != '#' and dist[nr][nc] is None:
-                    dist[nr][nc] = dist[r][c] + 1
-                    queue.append((nr, nc))
+                if ignore_walls or mapp[nr][nc] != '#':
+                    if dist[nr][nc] is None:
+                        new_cost = dist[r][c] + 1
+                        if max_steps is None or new_cost <= max_steps:
+                            dist[nr][nc] = new_cost
+                            queue.append((nr, nc))
     return dist
 
-def bfs_ignore_walls_limited(mapp, start, max_steps):
-    rows, cols = len(mapp), len(mapp[0])
-    dist = [[None]*cols for _ in range(rows)]
-    queue = deque()
-    r0, c0 = start
-    dist[r0][c0] = 0
-    queue.append((r0, c0))
-    while queue:
-        r, c = queue.popleft()
-        for dr, dc in DIRECTIONS:
-            nr, nc = r+dr, c+dc
-            if 0 <= nr < rows and 0 <= nc < cols:
-                if dist[nr][nc] is None:
-                    new_cost = dist[r][c] + 1
-                    if new_cost <= max_steps:
-                        dist[nr][nc] = new_cost
-                        queue.append((nr, nc))
-    return dist
 
-def find_track_cells(mapp):
+def find_all_track_cells(mapp):
     track = []
     rows, cols = len(mapp), len(mapp[0])
     for r in range(rows):
@@ -68,86 +56,47 @@ def find_track_cells(mapp):
                 track.append((r, c))
     return track
 
-def part1(mapp):
+
+def cheat_count(mapp, cheat_length, min_saving=100):
+
     S = find_item(mapp, 'S')
     E = find_item(mapp, 'E')
 
-    # 1) BFS for normal cost from S
-    dist_from_S = bfs_normal(mapp, S)
-    # 2) BFS for normal cost from E
-    dist_from_E = bfs_normal(mapp, E)
+    dist_from_S = bfs(mapp, S, ignore_walls=False, max_steps=None)
+    dist_from_E = bfs(mapp, E, ignore_walls=False, max_steps=None)
 
-    # Baseline no-cheat cost T0
     T0 = dist_from_S[E[0]][E[1]]
-    if T0 is None:
-        # If there's no path at all, puzzle is ill-posed or map is impossible
-        return 0
 
-    # Gather all track cells
-    track_cells = find_track_cells(mapp)
+    track_cells = find_all_track_cells(mapp)
+    ignore_bfs = {}
 
-    # For each track cell, precompute BFS ignoring walls (up to 2 steps).
-    # We'll store them in a dictionary: cheat_dist[(r, c)] = 2D-dist-array
-    cheat_dist_map = {}
     for cell in track_cells:
-        cheat_dist_map[cell] = bfs_ignore_walls_limited(mapp, cell, max_steps=2)
+        ignore_bfs[cell] = bfs(mapp, cell, ignore_walls=True, max_steps=cheat_length)
 
-    # Now enumerate all pairs of track cells (A, B).
-    # The cheat is: S -> A (normal), A -> B (ignore walls, <=2 steps), B -> E (normal).
-    # If the ignoring-walls distance is None or >2, skip.  Must also land on track again.
-    # Then saving = T0 - total_time.  If >= 100 => count that cheat.
-    # Distinct cheats are determined by (A, B).
     good_cheats = 0
     for A in track_cells:
-        aS = dist_from_S[A[0]][A[1]]  # cost S->A
-        if aS is None:
+        dist_s_a = dist_from_S[A[0]][A[1]]
+        if dist_s_a is None:
             continue
         for B in track_cells:
-            distAtoB = cheat_dist_map[A][B[0]][B[1]]
-            if distAtoB is None:  # can't cheat from A->B in <=2 steps
+            dist_a_b = ignore_bfs[A][B[0]][B[1]]
+            if dist_a_b is None:
                 continue
-            bE = dist_from_E[B[0]][B[1]]  # cost B->E
-            if bE is None:
+            dist_b_e = dist_from_E[B[0]][B[1]]
+            if dist_b_e is None:
                 continue
-            T_cheat = aS + distAtoB + bE
+            T_cheat = dist_s_a + dist_a_b + dist_b_e
             saving = T0 - T_cheat
-            if saving >= 100:
+            if saving >= min_saving:
                 good_cheats += 1
     return good_cheats
+
+
+def part1(mapp):
+    return cheat_count(mapp, cheat_length=2, min_saving=100)
 
 def part2(mapp):
-    S = find_item(mapp, 'S')
-    E = find_item(mapp, 'E')
-    dist_from_S = bfs_normal(mapp, S)
-    dist_from_E = bfs_normal(mapp, E)
-    T0 = dist_from_S[E[0]][E[1]]
-    if T0 is None:
-        return 0
-
-    track_cells = find_track_cells(mapp)
-
-    # Precompute ignoring-walls BFS up to 20 steps from each track cell
-    cheat_dist_map = {}
-    for cell in track_cells:
-        cheat_dist_map[cell] = bfs_ignore_walls_limited(mapp, cell, max_steps=20)
-
-    good_cheats = 0
-    for A in track_cells:
-        aS = dist_from_S[A[0]][A[1]]
-        if aS is None:
-            continue
-        for B in track_cells:
-            distAtoB = cheat_dist_map[A][B[0]][B[1]]
-            if distAtoB is None:
-                continue
-            bE = dist_from_E[B[0]][B[1]]
-            if bE is None:
-                continue
-            T_cheat = aS + distAtoB + bE
-            saving = T0 - T_cheat
-            if saving >= 100:
-                good_cheats += 1
-    return good_cheats
+    return cheat_count(mapp, cheat_length=20, min_saving=100)
 
 def solve(puzzle_input):
     data = parse_data(puzzle_input)
@@ -156,6 +105,7 @@ def solve(puzzle_input):
 
 if __name__ == "__main__":
     for path in sys.argv[1:]:
-        mapp = parse_data(pathlib.Path(path).read_text())
-        answers = solve(mapp)
+        raw_text = pathlib.Path(path).read_text()
+        data = parse_data(raw_text)
+        answers = solve(data)
         print(path, *answers)
